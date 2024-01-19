638ab35a01fa8bb022413bee7c3ccebf3d06023f
Luk1e <luka.gogiashvili.02@gmail.com>
2024-01-19 19:09:26 +0400

    created global styles & added respondTo helper function

--- tbc-project/src/App.tsx
+++ tbc-project/src/App.tsx
@@ -1,11 +1,17 @@
-import './App.css'
+import GlobalStyles from "./GlobalStyles";
 
-function App(){
+function App() {
+  document.addEventListener("DOMContentLoaded", function () {
+    const realWidth = window.screen.width * window.devicePixelRatio;
+    const realHeight = window.screen.height * window.devicePixelRatio;
+    console.log(`Your screen resolution is: ${realWidth} x ${realHeight}`);
+  });
   return (
-    <>
-      
-    </>
-  )
+    <div>
+      <GlobalStyles />
+      asd
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ tbc-project/src/utils/_respondTo.tsx
@@ -0,0 +1,32 @@
+// Import styled components css
+import { css } from "styled-components";
+
+// Breaks for media query
+const customBreakpoints = [
+  { label: "mobile", breakPoint: "0px" },
+  { label: "lowTablet", breakPoint: "480px" },
+  { label: "tablet", breakPoint: "767px" },
+  { label: "laptop", breakPoint: "1024px" },
+  { label: "desktop", breakPoint: "1280px" },
+  { label: "tv", breakPoint: "1920px" },
+];
+
+// Custom media query
+export const respondTo = customBreakpoints.reduce(
+  (accumulator, device, index) => {
+    const minWidth = parseInt(device.breakPoint, 10) + 1 + "px";
+    const maxWidth =
+      index + 1 === customBreakpoints.length
+        ? "10000px"
+        : customBreakpoints[index + 1].breakPoint;
+
+    accumulator[device.label] = (...args: any[]) => css`
+      @media (min-width: ${minWidth}) and (max-width: ${maxWidth}) {
+        ${args[0]}
+      }
+    `;
+
+    return accumulator;
+  },
+  {} as Record<string, (...args: any[]) => any>
+);
